679d775e7e66e3bbf9ccc0eb72b1f0ef168a5ba5
Pranto Deb <rajprit0m@outlook.com>
2025-07-27 20:53:47 +0600

    [User]

--- models/user.model.js
+++ models/user.model.js
@@ -1,21 +1,26 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // Hash password before save
 userSchema.pre("save", async function (next) {
