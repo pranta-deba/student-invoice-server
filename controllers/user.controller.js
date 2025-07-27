02f6f1cb0588ca56767a7541b7da9cfd07719f9f
Pranto Deb <rajprit0m@outlook.com>
2025-07-27 21:07:49 +0600

    [Register]

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,11 +6,14 @@ export const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
     const userExists = await User.findOne({ email });
     if (userExists)
-      return res.status(400).json({ message: "User already exists" });
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
 
     const user = await User.create({ name, email, password });
     const token = createToken(user);
     res.status(201).json({
+      success: true,
       token,
       user: {
         id: user._id,
@@ -21,7 +24,9 @@ export const registerUser = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
+    res
+      .status(500)
+      .json({ success: false, message: "Server error", error: error.message });
   }
 };
 
@@ -30,11 +35,14 @@ export const loginUser = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid email or password" });
     }
 
     const token = createToken(user);
     res.json({
+      success: true,
       token,
       user: {
         id: user._id,
@@ -45,6 +53,8 @@ export const loginUser = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
+    res
+      .status(500)
+      .json({ success: false, message: "Server error", error: error.message });
   }
 };
