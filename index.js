29c0df1cfb0d9c39db784fa727a48fbbdeafad2c
Pranto Deb <rajprit0m@outlook.com>
2025-07-30 19:16:38 +0600

    [Student]

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { dbConnect } from "./utils/dbConnect.js";
 import userRoutes from "./routes/user.route.js";
+import studentRoutes from "./routes/student.route.js";
 
+//! Config
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,11 +23,14 @@ dbConnect();
 
 //! Routes
 app.use("/api/users", userRoutes);
+app.use("/api/students", studentRoutes);
 
+//! Test
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//! Start server
 app.listen(port, () => {
   console.log(`🚀 Server is running on http://localhost:${port}`);
 });
